1293945cebf15664d293d8ea5484172bc9747236
vsparrow <khan@vsparrow.com>
2018-04-07 03:02:24 -0400

    makeDivOfAllLists() added.

--- src/index.js
+++ src/index.js
@@ -19,6 +19,38 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 });
+////////////////////////////////////////////////////////////////////////////////makeDivOfAllLists
+//called by displayLists
+function makeDivOfAllLists(){
+  let divOfLists = `<div id="lists">`
+  for( list in appMain.tasksList){
+    console.log("do we even get here?");
+    let divContent = `<div><h2>${list}<button data-title="${list}" class="delete-list">X</button></h2><ul>`
+    for( task of appMain.tasksList[list]){
+      console.log("task is:" +task)
+      console.log(task)
+      if(task){ 
+        let taskContent = `<li>"Task:${task['description']}"`
+        taskContent += `<button data-list-title="${list}" data-task-name="${task['description']}" class="delete-task">X</button>`
+        taskContent += `<br>"Priority:${task['priority']}"</li>`
+        divContent += taskContent
+      }
+    } //task
+    divContent += `</ul></div>`
+    divOfLists += divContent
+  }//for list
+  divOfLists +=`</div>`
+  return divOfLists
+}//makeDivOfAllLists
+////////////////////////////////////////////////////////////////////////////////displayLists
+//called by taskBuilder
+function displayLists(){
+  //get each list and its task description and priority
+  let divOfLists = makeDivOfAllLists()
+  //create div with id="lists"
+  //inner children should be the lists
+  //place div in div app-content  under form
+}
 ////////////////////////////////////////////////////////////////////////////////displayFormCreateTaskForm
 //called by taskBuilder
 function displayFormCreateTaskForm(formContent){
@@ -29,7 +61,7 @@ function displayFormCreateTaskForm(formContent){
 ////////////////////////////////////////////////////////////////////////////////createSelectOptionsFromTaskListKeys
 function createSelectOptionsFromTaskListKeys() {  //called by taskBuilder
   //from each tasklist name create a form list option value
-  console.log("createSelectOptionsFromTaskListKeys was called");
+  // console.log("createSelectOptionsFromTaskListKeys was called");
   let options = "";
   for(taskListName in appMain.tasksList){
       let option = "";
@@ -37,7 +69,7 @@ function createSelectOptionsFromTaskListKeys() {  //called by taskBuilder
       // if (options.length == 0){selected = `selected`}
       if(appMain.lastTaskListAdded === taskListName){selected = `selected`}
       option = `<option value="${taskListName}" ${selected}>${taskListName}</option>`
-      console.log(option)
+      // console.log(option)
       options += option
   }//for
   return options
@@ -45,7 +77,7 @@ function createSelectOptionsFromTaskListKeys() {  //called by taskBuilder
 ////////////////////////////////////////////////////////////////////////////////taskBuilder
 //display form for adding tasks to tasks lists.
 function taskBuilder(){
-  console.log("taskBuilder was called");
+  // console.log("taskBuilder was called");
   //<div id="app-content"> get child node of <form id="create-task-form">
   let formCreateTaskForm=`<form id="create-task-form">
     <label for="parent-list">Select List:</label>
@@ -62,19 +94,21 @@ function taskBuilder(){
   </form>`
   // createSelectOptionsFromTaskListKeys()
   displayFormCreateTaskForm(formCreateTaskForm);
+  //displayLists()
+  //get input from "create-task-form" submission, add to list, display
 }//taskBuilder
 
 ////////////////////////////////////////////////////////////////////////////////createNewTaskList
  //add variable name as key to the appMain.tasksList hash
 function createNewTaskList(name){
-  console.log("In createNewTaskList");
-  console.log("name is:" +name);
+  // console.log("In createNewTaskList");
+  // console.log("name is:" +name);
   // console.log(appMain.tasks[0].description)
   // console.log(appMain.tasks[0])
   // this.tasks = {"thisIsATestTaskList": [{description: "someTestTask", priority: 5}] };
   appMain.tasksList[name]=[];
   appMain.lastTaskListAdded=name;
-  console.log(appMain.tasksList)
+  // console.log(appMain.tasksList)
   //now call on taskBuilder. no neeed to pass a variable
   taskBuilder()
 }//createNewTaskList
@@ -87,7 +121,7 @@ function getNewTaskListName(){
   // if empty alert user
   if(newListName.length == 0){alert("Tasklist title cannot be empty!")}
   else{
-    console.log(newListName)
+    // console.log(newListName)
     createNewTaskList(newListName)//generateListEntries
   }//else
 }//getNewTaskListName
