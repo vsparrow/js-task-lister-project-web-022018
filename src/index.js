0cb6c9ed191b9f8553bd02904d45403e5a3608b9
vsparrow <khan@vsparrow.com>
2018-04-07 03:36:59 -0400

    create addEventListenerForCreateTaskForm()

--- src/index.js
+++ src/index.js
@@ -19,17 +19,33 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 });
+////////////////////////////////////////////////////////////////////////////////getNewTask
+//called by addEventListenerForCreateTaskForm
+function getNewTask() {
+  //get value from form
+  // update tasklist
+  //update display
+}
+////////////////////////////////////////////////////////////////////////////////addEventListenerForCreateTaskForm
+//called by taskBuilder
+function addEventListenerForCreateTaskForm() {
+  document.getElementById("submit-create-task-form").addEventListener("click", function(event) {
+     event.preventDefault();
+     console.log("SUBMIT {PRESSSED")
+     getNewTask()
+  })
+}//addEventListenerForCreateTaskForm
 ////////////////////////////////////////////////////////////////////////////////makeDivOfAllLists
 //called by displayLists
 function makeDivOfAllLists(){
   let divOfLists = `<div id="lists">`
   for( list in appMain.tasksList){
-    console.log("do we even get here?");
+    // console.log("do we even get here?");
     let divContent = `<div><h2>${list}<button data-title="${list}" class="delete-list">X</button></h2><ul>`
     for( task of appMain.tasksList[list]){
-      console.log("task is:" +task)
-      console.log(task)
-      if(task){ 
+      // console.log("task is:" +task)
+      // console.log(task)
+      if(task){
         let taskContent = `<li>"Task:${task['description']}"`
         taskContent += `<button data-list-title="${list}" data-task-name="${task['description']}" class="delete-task">X</button>`
         taskContent += `<br>"Priority:${task['priority']}"</li>`
@@ -46,10 +62,14 @@ function makeDivOfAllLists(){
 //called by taskBuilder
 function displayLists(){
   //get each list and its task description and priority
-  let divOfLists = makeDivOfAllLists()
   //create div with id="lists"
   //inner children should be the lists
+  let divOfLists = makeDivOfAllLists()
   //place div in div app-content  under form
+  content = document.querySelector('#app-content').innerHTML
+  content+= divOfLists
+  document.querySelector('#app-content').innerHTML = content
+  //now add addEventListener to each button created*****************************************************
 }
 ////////////////////////////////////////////////////////////////////////////////displayFormCreateTaskForm
 //called by taskBuilder
@@ -90,12 +110,14 @@ function taskBuilder(){
 
     <label for="new-task-priority">Priority level:</label>
     <input type="text" id="new-task-priority" placeholder="priority">
-    <input type="submit" value="Create New Task">
+    <input type="submit" value="Create New Task" id="submit-create-task-form">
   </form>`
   // createSelectOptionsFromTaskListKeys()
   displayFormCreateTaskForm(formCreateTaskForm);
-  //displayLists()
+  displayLists()
   //get input from "create-task-form" submission, add to list, display
+    //add addEventListener? preventDefault to submit button
+    addEventListenerForCreateTaskForm()
 }//taskBuilder
 
 ////////////////////////////////////////////////////////////////////////////////createNewTaskList
@@ -131,8 +153,9 @@ class TaskLister {
   // your solution here
   constructor(){
     this.test = "test";
-    this.tasksList = {"thisIsATestTaskList": [{description: "someTestTask", priority: 5}] };
-    this.lastTaskListAdded = "";
+    this.tasksList = {"thisIsATestTaskList": [{description: "someTestTask", priority: 5}] }; //test data remove after
+    // this.tasksList = {};
+    this.lastTaskListAdded = "";  //if referenced list removed, some function assign  first task to this *********
   }//end constructor
 
   render() {
